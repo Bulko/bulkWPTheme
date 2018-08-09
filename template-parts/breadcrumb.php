13a346bf17a9e1d1e408b20d021e718d162170be
Golga <Golgarud@users.noreply.github.com>
2018-08-09 10:22:01 +0200

    CORE Yoast breadcrumb

---
+++ template-parts/breadcrumb.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template part for displaying results in search pages
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package agc-2017
+ */
+
+if ( function_exists('yoast_breadcrumb') )
+{
+	yoast_breadcrumb('<div class="breadcrumbs" >','</div>');
+}
+?>
