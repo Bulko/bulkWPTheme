8ccf34f1cb10caaf705b2b13f04efbd2564e4ae2
Noumie <Noumie@users.noreply.github.com>
2018-08-09 10:18:43 +0200

    Update content-page.php

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -8,6 +8,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php include '_breadcrumb.php' ?>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
