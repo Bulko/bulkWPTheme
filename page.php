51e0dde0b8a40349d1d99ace8ec2e553865e7d52
Noumie <Noumie@users.noreply.github.com>
2018-08-09 10:13:28 +0200

    Update page.php

--- page.php
+++ page.php
@@ -14,20 +14,18 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
 			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
+			if ( is_home() || is_front_page() )
+			{
+				get_template_part( 'template-parts/content', 'home' );
+			}
+			else
+			{
+				while ( have_posts() ) : the_post();
+					get_template_part( 'template-parts/content', 'page' );
+				endwhile; // End of the loop.
+			}
 			?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
